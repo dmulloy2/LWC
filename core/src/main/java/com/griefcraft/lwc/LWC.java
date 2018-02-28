00bf273b3636eb129a6330411ca232b00c315010
Phoenix616 <mail@moep.tv>
2018-02-28 14:40:22 +0100

    Add hopper protection that is not based on the InventoryMoveItemEvent This alternative protection mode checks the container above and the one a hopper is pointing into when a hopper is placed. Only hopper minecarts still rely on the move item event.

--- core/src/main/java/com/griefcraft/lwc/LWC.java
+++ core/src/main/java/com/griefcraft/lwc/LWC.java
@@ -210,11 +210,17 @@ public class LWC {
      */
     private boolean fastHoppers;
 
+    /**
+     * Whether alternative-hopper-protection is enabled
+     */
+    private boolean alternativeHoppers;
+
     public LWC(LWCPlugin plugin) {
         this.plugin = plugin;
         LWC.instance = this;
         configuration = Configuration.load("core.yml");
         fastHoppers = configuration.getBoolean("optional.fastHopperProtection", false);
+        alternativeHoppers = configuration.getBoolean("optional.alternativeHopperProtection", false);
         protectionCache = new ProtectionCache(this);
         backupManager = new BackupManager();
         moduleLoader = new ModuleLoader(this);
@@ -1903,6 +1909,7 @@ public class LWC {
         protectionConfigurationCache.clear();
         Configuration.reload();
         fastHoppers = configuration.getBoolean("optional.fastHopperProtection", false);
+        alternativeHoppers = configuration.getBoolean("optional.alternativeHopperProtection", false);
         moduleLoader.dispatchEvent(new LWCReloadEvent());
     }
 
@@ -2053,8 +2060,14 @@ public class LWC {
     /**
      * @return true if fast hopper protection is enabled
      */
-    public boolean useFastHopperProtection()
-    {
+    public boolean useFastHopperProtection() {
         return fastHoppers;
+    }
+
+    /**
+     * @return true if alternative hopper protection is enabled
+     */
+    public boolean useAlternativeHopperProtection() {
+        return alternativeHoppers;
     }
 }
